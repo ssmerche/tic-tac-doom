13381d164d718038ffa57d85fb443f873f1f65f5
Scott Smerchek <ssmerche@gmail.com>
2012-11-30 10:30:18 -0800

    use filter in winner function instead of map

--- src/tic_tac_doom/core.clj
+++ src/tic_tac_doom/core.clj
@@ -1,19 +1,19 @@
 (ns tic-tac-doom.core)
 
 (defn column-sets [board]
-  (->> board (apply interleave) (partition (count (first board))) (map set)))
+ (for [i (range (count board))] (set (map #(% i) board))))
 
 (def row-sets (partial map set))
 
-(defn winner [sets]
-  (->> sets (map {#{:x} :x, #{:o} :o}) (remove nil?) first))
-
-(defn diag-sets [board]
+(defn diagonal-sets [board]
   [(set (map #(%1 %2) board (range (count board)))) 
    (set (map #(%1 %2) board (reverse (range (count board)))))])
+
+(def board-sets (juxt column-sets row-sets diagonal-sets))
+
+(defn find-winner [sets] (->> sets (filter #{#{:x} #{:o}}) ffirst))
 
 (defn analyze
   "Determine the winner of a game of tic-tac-toe."
   [board]
-  (->> (map winner [(column-sets board) (row-sets board) (diag-sets board)]) 
-    (remove nil?) first))
+  (->> (board-sets board) (map find-winner) (remove nil?) first))
